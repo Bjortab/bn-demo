42d37c192f66734246af61be24aabc1bb582be98
Bjortab <bjorta.b@icloud.com>
2025-08-13 15:31:22 +0200

    Delete functions/api directory

--- functions/api/hello.js
+++
@@ -1,5 +0,0 @@
-export async function onRequest({ request }) {
-  const url = new URL(request.url);
-  const name = url.searchParams.get('name') || 'världen';
-  return new Response(`Hej ${name}!`, { headers: { 'content-type': 'text/plain' }});
-}
