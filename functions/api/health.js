1e7c8b7e5dcf4f9db7a60863668fcb891b83be1b
Bjortab <bjorta.b@icloud.com>
2025-08-15 15:28:16 +0200

    Update health.js

--- functions/api/health.js
+++ functions/api/health.js
@@ -1,9 +1,8 @@
-// functions/api/health.js
 export async function onRequestGet() {
-  return new Response(JSON.stringify({ ok: true, time: new Date().toISOString() }), {
+  return new Response('ok', {
     headers: {
-      "Content-Type": "application/json; charset=utf-8",
-      "Access-Control-Allow-Origin": "*",
-    },
+      'Cache-Control': 'no-store',
+      'Content-Type': 'text/plain; charset=utf-8',
+    }
   });
 }
