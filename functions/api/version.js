60df67cc9f7c32ba66dae717a8ffbbd1c0f29485
Bjortab <bjorta.b@icloud.com>
2025-08-27 08:47:43 +0200

    Create version.js

---
+++ functions/api/version.js
@@ -0,0 +1,12 @@
+import { jsonResponse, corsHeaders } from "./_utils.js";
+
+export async function onRequest({ request, env }) {
+  const info = {
+    ok: true,
+    ts: Date.now(),
+    commit: env?.CF_PAGES_COMMIT_SHA || null,
+    branch: env?.CF_PAGES_BRANCH || null,
+    project: env?.CF_PAGES_PROJECT_NAME || null,
+  };
+  return jsonResponse(info, 200, corsHeaders(request));
+}
