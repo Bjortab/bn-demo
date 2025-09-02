a89e7789040ab1c295e81ad9e107369bdafe324a
Bjortab <bjorta.b@icloud.com>
2025-09-02 08:21:38 +0200

    Delete config.js

--- config.js
+++
@@ -1,7 +0,0 @@
-// config.js (Golden Copy)
-
-window.BN_CONFIG = {
-  apiBase: location.origin + "/api",   // Alla API-anrop går hit
-  version: "v1.2",                     // Frontend version – visas nere i foten
-  debug: true                          // Slå på/av konsolloggar
-};
